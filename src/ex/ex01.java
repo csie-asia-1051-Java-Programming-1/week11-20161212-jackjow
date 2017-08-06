b9682221ea0ef34cec949f4b40b281e2a94d1170
jackjow <30774735+jackjow@users.noreply.github.com>
2017-08-06 23:27:33 +0800

    My first commit

--- src/ex/ex01.java
+++ src/ex/ex01.java
@@ -5,6 +5,6 @@ package ex;
  */
 public class ex01 {
 	public static void main(String[] args) {
-
+		System.out.println("This is a test");
 	}
 }
--- src/ex/ex03.java
+++ src/ex/ex03.java
@@ -5,6 +5,6 @@ package ex;
  */
 public class ex03 {
 	public static void main(String[] args) {
-
+		System.out.println("This is a test3");
 	}
 }
